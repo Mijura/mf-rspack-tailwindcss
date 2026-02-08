3cf2c71a2c2afd41d8260b35dfeeb20f6ae80b9b
Mijura <miodragvilotijevic@gmail.com>
2026-02-08 15:44:47 +0100

    moved const value directly to mf plugin

--- configs/rspack.base.js
+++ configs/rspack.base.js
@@ -4,24 +4,6 @@ const Dotenv = require("dotenv-webpack")
 const { TsCheckerRspackPlugin } = require("ts-checker-rspack-plugin")
 const { ModuleFederationPlugin, createModuleFederationConfig } = require("@module-federation/enhanced/rspack")
 
-const mfConfig = createModuleFederationConfig({
-	name: "host",
-
-	exposes: {
-		"./App": "./src/App.tsx",
-	},
-	shared: {
-		react: {
-			singleton: true,
-			eager: true,
-		},
-		"react-dom": {
-			singleton: true,
-			eager: true,
-		},
-	},
-})
-
 // Target browsers, see: https://github.com/browserslist/browserslist
 const targets = ["chrome >= 87", "edge >= 88", "firefox >= 78", "safari >= 14"]
 
@@ -76,7 +58,25 @@ module.exports = {
 			path: "./.env",
 			safe: true,
 		}),
-		new ModuleFederationPlugin(mfConfig),
+		new ModuleFederationPlugin(
+			createModuleFederationConfig({
+				name: "host",
+
+				exposes: {
+					"./App": "./src/App.tsx",
+				},
+				shared: {
+					react: {
+						singleton: true,
+						eager: true,
+					},
+					"react-dom": {
+						singleton: true,
+						eager: true,
+					},
+				},
+			})
+		),
 		new TsCheckerRspackPlugin({
 			typescript: {
 				configOverwrite: {
